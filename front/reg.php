ec638feafe115bf8a9d3870be3c8da4d707992ab
cable <cable870526@gmail.com>
2024-01-25 15:05:57 +0800

    註冊字元限制

--- front/reg.php
+++ front/reg.php
@@ -2,13 +2,13 @@
     <legend>會員註冊</legend>
     <h3 style="color: red;">*請設定您要註冊的帳號及密碼(最⾧12個字元)</h3>
     <label for="acc">帳號:</label>
-    <input type="text" name="acc" id="acc">
+    <input type="text" name="acc" id="acc" maxlength="12">
     <br>
     <label for="pw">密碼:</label>
-    <input type="text" name="pw" id="pw">
+    <input type="text" name="pw" id="pw" maxlength="12">
     <br>
     <label for="pw2">確認密碼:</label>
-    <input type="text" name="pw2" id="pw2">
+    <input type="text" name="pw2" id="pw2" maxlength="12">
     <br>
     <label for="email">信箱:</label>
     <input type="text" name="email" id="email">
